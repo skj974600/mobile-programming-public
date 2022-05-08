2b6301bda280a358ba6bfc198922fd058126062d
yureutaejin <jyjin3@me.com>
2022-05-08 18:51:20 +0900

    add manifest,serviceworker for splashscreen

--- vue-mobile-todo/src/main.js
+++ vue-mobile-todo/src/main.js
@@ -2,17 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
-import * as VueGoogleMaps from "vue2-google-maps" // Import package
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyCCi_7tznaOyieARc7XxL4RgUgahQT-4Cs",
-    libraries: "places",
-    region: "KR"
-  }
-});
 
 new Vue({
   vuetify,
